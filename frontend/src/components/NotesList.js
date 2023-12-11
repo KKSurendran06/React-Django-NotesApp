b829ec85285575cd2fdb3b2067d295c00cd3f1fa
KKSurendran06 <kksurendran95@gmail.com>
2023-12-11 18:03:16 +0530

    init

---
+++ frontend/src/components/NotesList.js
@@ -0,0 +1,25 @@
+import Note from "./Notes";
+import AddNote from "./AddNote"
+
+const NotesList = ({ notes , handleAddNote, handleDeleteNote, getNotes}) => {
+    
+    return(
+        
+        <div className="notes-l">
+            {notes.map((notes) => (
+                <Note id ={notes.id} 
+                      text={notes.text} 
+                      date={notes.date}
+                      duedate={notes.duedate} 
+                      dead = {notes.dead}
+                      handleDeleteNote = {handleDeleteNote}
+                      getNotes={getNotes}/>
+            ))}
+        <AddNote handleAddNote = {handleAddNote} />
+        </div>
+
+        
+    );
+};
+
+export default NotesList
